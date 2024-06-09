4963aa6f3db76d0e9c9d3c062c5f74e233c0a55e
me <me@envy-me.me>
2024-06-09 17:10:16 -0400

    optimize searching speed

---
+++ app/src/components/MedicalJournals.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Medical Journals</h1>
+        <div>
+            {{ Vet_info_group_1 }}
+
+            <hr>
+
+            {{ Vet_info_group_2 }}
+
+            <hr>
+
+            {{ Vet_info_group_3 }}
+
+            <hr>
+
+            {{ Medical Records }}
+        </div>
+    </div>
+</template>
