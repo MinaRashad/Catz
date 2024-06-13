2c4773453d24f62815da59b728dcabf23bde7097
me <me@envy-me.me>
2024-06-13 15:15:05 -0400

    retrieved journal types

--- app/src/views/JournalUpload.vue
+++ app/src/views/JournalUpload.vue
@@ -1,8 +1,10 @@
 <template>
+
     <router-link :to="{name:'catDetails'}">
         <h1>{{ '<<' }}</h1>
     </router-link>
-    <h2>Journal Upload</h2>
+    <div v-show="journal_types_loaded">    
+        <h2>Journal Upload</h2>
     <div>
         <label>Initials (4 letters max)</label><input type="text" v-model="initials" maxlength="4">
         <label>Number of entries</label><input type="number" v-model="numEntries">
@@ -24,22 +26,29 @@
             <tr v-for="n in numEntries" :key="n">
                 <td><input type="date" v-model="journals[n-1].journalEntryDate"></td>
                 <td><select v-model="journals[n-1].journalEntrytypeDescription">
-                    <option v-for='type in ["De Wormer 1", "De Wormer 2"
-                    , "FVRCP", "Rabies", "FIV/FeLV", "Microchip", "Weight", "Medication", "Blood", "Medical", "Fecal", "GAP Funding", "Vet"
-                    ]' :key="type">{{ type }}</option>
-                </select></td>
+                    <option v-for='type in journalTypes' :key="type.name">{{ type.name }}</option>
+                </select>
+                </td>
                 <td><input type="textarea" v-model="journals[n-1].journalEntryComment"></td>
                 <td><input type="date" v-model="journals[n-1].journalEntryDueDate"></td>
                 <td><input type="date" v-model="journals[n-1].journalEntryReminderDate"></td>
                 <td><input type="number" v-model="journals[n-1].journalEntryCost"></td>
             </tr>
+            <button>Submit</button>
         </tbody>
     </table>
-    
+</div>
+<div v-show="!journal_types_loaded">
+    <h2>Loading...</h2>
+</div>
+
+
 </template>
 
 
 <script>
+
+
     export default {
         name: 'JournalUpload',
         data(){
@@ -55,13 +64,21 @@
                     journalEntryDueDate: '',
                     journalEntryReminderDate: '',
                     journalEntryCost: ''
-                }
+                },
+                journalTypes: [],
+                journal_types_loaded:false
             }
+        },
+        mounted: async function(){
+            const response = await fetch('http://localhost:3000/journal_types');
+            this.journalTypes = await response.json();
+            this.journal_types_loaded = true;
+
         },
         methods:{
             addJournals(){
                 this.journals = [];
-                this.journalTemplate.journalEntryComment = this.initials+': ';
+                this.journalTemplate.journalEntryComment = this.initials.toLocaleUpperCase()+': ';
                 for(let i=0; i<this.numEntries; i++)
                 this.journals.push({...this.journalTemplate});
             }
